888ddc8a254723083c4edef0c82251eae8b0610b
Sagnnik Biswas <sagnnikbiswas2002@gmail.com>
2022-09-18 16:54:32 +0530

    DCGAN model

---
+++ DCGAN/model.py
@@ -0,0 +1,63 @@
+import torch
+import torch.nn as nn
+
+class Discriminator(nn.Module):
+    def __init__(self, channels_img, features_d):
+        super(Discriminator, self).__init__()
+        self.disc = nn.Sequential(
+            nn.Conv2d(channels_img, features_d, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2),
+            self.block(features_d, features_d * 2, 4, 2, 1),
+            self.block(features_d * 2, features_d * 4, 4, 2, 1),
+            self.block(features_d * 4, features_d * 8, 4, 2, 1),
+            nn.Conv2d(features_d * 8, 1, kernel_size=4, stride=2, padding=0),
+            nn.Sigmoid(),
+        )
+
+    def block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False),
+            nn.LeakyReLU(0.2),
+        )
+
+    def forward(self, x):
+        return self.disc(x)
+
+
+class Generator(nn.Module):
+    def __init__(self, channels_noise, channels_img, features_g):
+        super(Generator, self).__init__()
+        self.net = nn.Sequential(
+            self.block(channels_noise, features_g * 16, 4, 1, 0),  # img: 4x4
+            self.block(features_g * 16, features_g * 8, 4, 2, 1),  # img: 8x8
+            self.block(features_g * 8, features_g * 4, 4, 2, 1),  # img: 16x16
+            self.block(features_g * 4, features_g * 2, 4, 2, 1),  # img: 32x32
+            nn.ConvTranspose2d(
+                features_g * 2, channels_img, kernel_size=4, stride=2, padding=1
+            ),
+            nn.Tanh(),
+        )
+
+    def block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, bias=False ),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+def initialize_weights(model):
+    for m in model.modules():
+        if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
+
+def test():
+    noise_dim = 100
+    x = torch.randn((1, 3, 64, 64))
+    disc = Discriminator(3, 8)
+    print(disc(x).shape)
+    gen = Generator(noise_dim, 3, 8)
+    z = torch.randn((1, noise_dim, 1, 1))
+    print(gen(z).shape)
+
+#test()
