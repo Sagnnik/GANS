9885fc2a293259acfd53f2c12ffe01a69ee09876
Sagnnik Biswas <sagnnikbiswas2002@gmail.com>
2022-09-18 17:21:38 +0530

    WGAN implementation from scratch

--- WGAN/model.py
+++ WGAN/model.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 class Discriminator(nn.Module):
     def __init__(self, channels_img, features_d):
         super(Discriminator, self).__init__()
-        # 3x64x64
+        # Input: 3 x 64 x 64
         self.disc = nn.Sequential(nn.Conv2d(channels_img, features_d, kernel_size=4, stride=2, padding=1),
             nn.LeakyReLU(0.2),
             self.block(features_d, features_d * 2, 4, 2, 1),
@@ -28,7 +28,7 @@ class Generator(nn.Module):
     def __init__(self, channels_noise, channels_img, features_g):
         super(Generator, self).__init__()
         self.net = nn.Sequential(
-            # Input: N x channels_noise x 1 x 1
+            # Input: batch_size x channels_noise x 1 x 1
             self.block(channels_noise, features_g * 16, 4, 1, 0),  # img: 4x4
             self.block(features_g * 16, features_g * 8, 4, 2, 1),  # img: 8x8
             self.block(features_g * 8, features_g * 4, 4, 2, 1),  # img: 16x16
@@ -36,7 +36,6 @@ class Generator(nn.Module):
             nn.ConvTranspose2d(
                 features_g * 2, channels_img, kernel_size=4, stride=2, padding=1
             ),
-            # Output: N x channels_img x 64 x 64
             nn.Tanh(),
         )
 
